a4c20706941c77573dbda532450ddf264fc36095
JordyNtambwe <jordyntambwe@gmail.com>
2024-03-01 19:01:30 -0700

    commit on master

--- tail.js
+++ tail.js
@@ -20,7 +20,9 @@ assertEqual(result[1], "Labs"); // ensure second element is "Labs"
 assertEqual(tail(["test"]).length, 0) // tail of array with one element only should be empty
 assertEqual(tail([]).length, 0) // tail of an empty array should also be empty
 
+
 // Test Case: Check the original array 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
+assertEqual(tail(words).length, words.length - 1); // tail should have one element less than its input
