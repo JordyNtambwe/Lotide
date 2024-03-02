31efd5cc265825b2a8cee729ebcdd52ebd653935
JordyNtambwe <jordyntambwe@gmail.com>
2024-03-01 18:57:25 -0700

    commit on master

--- tail.js
+++ tail.js
@@ -16,11 +16,7 @@ assertEqual(result.length, 2); // ensure we get back two elements
 assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
 assertEqual(result[1], "Labs"); // ensure second element is "Labs"
 
-// TEST CODE #2
+// Test Case: Check the original array 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
-
-// TEST CODE #3
-assertEqual(tail(["test"]).length, 0);
-assertEqual(tail([]).length, 0);
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
