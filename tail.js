25794bc89de82262111528f86d9288e914776e64
JordyNtambwe <jordyntambwe@gmail.com>
2024-03-01 18:33:09 -0700

    commit on master

--- tail.js
+++ tail.js
@@ -12,7 +12,7 @@ const tail = function(array) {
 
 // TEST CODE #1
 const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result.length - 1, 2); // ensure we get back two elements
 assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
 assertEqual(result[1], "Labs"); // ensure second element is "Labs"
 
