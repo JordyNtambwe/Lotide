c421738830b74fb1579452d5544e7b682f353811
JordyNtambwe <jordyntambwe@gmail.com>
2024-03-03 13:25:23 -0700

    commit on master

---
+++ assertArraysEqual.js
@@ -0,0 +1,26 @@
+const eqArrays = function (array1, array2) {
+
+    if (!array1 || !array2) {
+    return false;
+    }
+  
+    const sameLength = array1.length === array2.length;
+    if (!sameLength) {
+        return false;
+    }
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+const assertArrayEqual = function (array1, array2) {
+    
+    if (array1 === array2) {
+        console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`)
+    } else {
+        console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+    }
+}
