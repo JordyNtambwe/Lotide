0136a023c22bfcb6a82f33a677fa8b2819c0fa9b
JordyNtambwe <jordyntambwe@gmail.com>
2024-03-03 13:37:51 -0700

    commit on master

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -25,8 +25,8 @@ const assertArraysEqual = function (array1, array2) {
     }
 }
 // TEST CODE
-assertArraysEqual(([1, 2, 3], [1, 2, 3]), true)
-assertArraysEqual(([1, 2, 3], [1, 2, 4]), false)
-assertArraysEqual(([], []), true)
-assertArraysEqual((["test", "123"], ["test", "123"]), true)
-assertArraysEqual((["test", "123"], ["tes", "123"]), false)
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // should pass
+assertArraysEqual([1, 2, 3], [1, 2, 4]); // should fail
+assertArraysEqual([], []); // should pass
+assertArraysEqual(["test", "123"], ["test", "123"]); // should pass
+assertArraysEqual(["test", "123"], ["tes", "123"]);
