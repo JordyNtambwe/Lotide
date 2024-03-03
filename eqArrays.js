0513a041c3d1ff6ead6ed345d6cdddfe14875d47
JordyNtambwe <jordyntambwe@gmail.com>
2024-03-03 13:15:58 -0700

    commit on master

--- eqArrays.js
+++ eqArrays.js
@@ -29,3 +29,6 @@ const assertEqual = function(actual, expected) {
   // TEST CODE
   assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
   assertEqual(eqArrays([1, 2, 3], [1, 2, 4]), false)
+  assertEqual(eqArrays([], []), true)
+  assertEqual(eqArrays(["test", "123"], ["test", "123"]), true)
+  assertEqual(eqArrays(["test", "123"], ["tes", "123"]), false)
