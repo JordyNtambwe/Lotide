03e8faea0714575cc0a0ccd72840ac79b105735b
JordyNtambwe <jordyntambwe@gmail.com>
2024-03-03 22:52:21 -0700

    commit on master

---
+++ middle.js
@@ -0,0 +1,53 @@
+const assertArraysEqual = function (array1, array2) {
+
+    if (eqArrays(array1, array2)) {
+        console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`)
+    } else {
+        console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+    }
+}
+
+const eqArrays = function (array1, array2) {
+
+    if (!array1 || !array2) {
+    return false;
+    }
+  
+    const sameLength = array1.length === array2.length;
+    if (!sameLength) {
+        return false;
+    }
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
+    
+  };
+const middle = function(array) {
+
+  if(!array.length) {
+    return [];
+  }
+
+  const length = array.length;
+  const middleIndex = Math.floor(length/2);
+
+  if(length % 2 === 0) {
+    return [array[middleIndex - 1], array[middleIndex]]
+  } else {
+    return [array[middleIndex]]
+  }
+  
+}
+	
+
+// TEST CODE
+
+assertArraysEqual(middle([1]), []) 
+assertArraysEqual(middle([1, 2]), [])
+assertArraysEqual(middle([1, 2, 3]), [2])
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3])
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3])
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])
