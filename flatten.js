ca54a4b2e97adee851caf89ae73d744517d81d91
JordyNtambwe <jordyntambwe@gmail.com>
2024-03-03 18:43:47 -0700

    commit on master

---
+++ flatten.js
@@ -0,0 +1,43 @@
+const assertArraysEqual = function (array1, array2) {
+
+    if (eqArrays(array1, array2)) {
+        console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`)
+    } else {
+        console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+    }
+}
+
+const eqArrays = function (array1, array2) {
+
+    if (!array1 || !array2) {
+    return false;
+    }
+  
+    const sameLength = array1.length === array2.length;
+    if (!sameLength) {
+        return false;
+    }
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
+    
+  };
+
+const flatten = function(array) {
+
+	let newArray = []
+
+	for(let i = 0; i < array.length; i++) {
+		if(Array.isArray(array[i])) {
+			newArray = newArray.concat(flatten(array[i]));
+		} else {
+			newArray.push(array[i])
+		}
+	}
+	return newArray
+}
+
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5 ,6]) // => [1, 2, 3, 4, 5, 6]
