e1627d358f1a5d861c321e868a1ea82c77bece86
JordyNtambwe <jordyntambwe@gmail.com>
2024-03-03 17:15:56 -0700

    commit on master

--- without.js
+++ without.js
@@ -42,3 +42,6 @@ return arrayFinal
 //Test Code
 assertArraysEqual(without([1, 2, 3], [1]), [2,3]) // => [2, 3]
 assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]) // => ["1", "2"]
+
+const words = ["hello", "world", "lighthouse"];
+assertArraysEqual(words, ["hello", "world", "lighthouse"]); // => should PASS
