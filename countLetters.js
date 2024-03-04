4e6200ed590257847f9cc225223038c15220e5d5
JordyNtambwe <jordyntambwe@gmail.com>
2024-03-04 04:51:54 -0700

    commit on master

--- countLetters.js
+++ countLetters.js
@@ -23,7 +23,15 @@ const countLetters = function(string) {
   return results
 }
 
-// TEST CODES
+// TEST CODES #1
 const result1 = countLetters('LHL');
 assertEqual(result1['L'], 2);
 assertEqual(result1['H'], 1);
+
+// TEST CODES #2
+const result2 = countLetters('');
+assertEqual(Object.keys(result2).length, 0);
+
+// TEST CODES #3
+const result3 = countLetters('   ');
+assertEqual(Object.keys(result3).length, 0);
