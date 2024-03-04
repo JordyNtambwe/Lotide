28f1d436066c88eef2c11196c1dda65242bbb921
JordyNtambwe <jordyntambwe@gmail.com>
2024-03-03 17:12:26 -0700

    Co-authored-by: Shaun Purslow <shaund16@users.noreply.github.com>

--- without.js
+++ without.js
@@ -7,3 +7,38 @@ const assertArraysEqual = function (array1, array2) {
     }
 };
 
+const eqArrays = function (array1, array2) {
+
+    if (!array1 || !array2) {
+    return false;
+    }
+  
+    const sameLength = array1.length === array2.length;
+    if (!sameLength) {
+        return false;
+    }
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
+    
+  };
+
+const without = function(source, itemsToRemove) {
+
+  let arrayFinal =  [...source]
+  for(let i = 0; i < itemsToRemove.length; i++) {
+    for(let j = 0; j < source.length; j++) {
+        if(itemsToRemove[i] === arrayFinal[j]) {
+            arrayFinal.splice(j,1)
+        }
+    }
+  }
+return arrayFinal
+}
+
+//Test Code
+assertArraysEqual(without([1, 2, 3], [1]), [2,3]) // => [2, 3]
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]) // => ["1", "2"]
