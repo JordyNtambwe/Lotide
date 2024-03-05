1746b460842571052a5f9f1e0bdc87b04c06a19f
JordyNtambwe <jordyntambwe@gmail.com>
2024-03-04 18:01:37 -0700

    commit on master

---
+++ eqObjects.js
@@ -0,0 +1,60 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+  
+};
+
+const eqArrays = function (array1, array2) {
+
+  if (!array1 || !array2) {
+  return false;
+  }
+
+  const sameLength = array1.length === array2.length;
+  if (!sameLength) {
+      return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+  
+};
+
+const eqObjects = function(object1, object2) {
+
+for(const keys of Object.keys(object1)) {
+  if(Array.isArray(Object.keys(object1)) !== Array.isArray(Object.keys(object2))) {
+    return false
+  } else {
+    return true
+  }
+}
+
+}
+
+// TEST CODE
+
+// const shirtObject = { color: "red", size: "medium" };
+// const anotherShirtObject= { size: "medium", color: "red" };
+// eqObjects(shirtObject , anotherShirtObject); // => true
+// assertEqual(eqObjects(shirtObject , anotherShirtObject), true);
+
+// const longSleeveShirtObject= { size: "medium", color: "red", sleeveLength: "long" };
+// eqObjects(shirtObject , longSleeveShirtObject); // => false
+// assertEqual(eqObjects(shirtObject , longSleeveShirtObject), false);
+
+const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
+const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
+eqObjects(multiColorShirtObject  , anotherMultiColorShirtObject); // => true
+assertEqual(eqObjects(multiColorShirtObject  , anotherMultiColorShirtObject)); // => true
+
+const longSleeveMultiColorShirtObject= { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
+eqObjects(multiColorShirtObject  , longSleeveMultiColorShirtObject); // => false
+assertEqual(eqObjects(multiColorShirtObject  , longSleeveMultiColorShirtObject)); // => false
