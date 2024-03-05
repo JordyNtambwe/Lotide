6269bf7ed8c8d6dc66b7e0c839577f1e0351e9ca
JordyNtambwe <jordyntambwe@gmail.com>
2024-03-05 15:54:22 -0700

    commit on master

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -21,7 +21,7 @@ const eqObjects = function(object1, object2) {
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect;
 
-  if (eqArrays(array1, array2)) {
+  if (eqObjects(actual, expected)) {
     console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`)
 } else {
     console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
