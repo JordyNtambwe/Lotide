99edae9291f5456f7ce0d0d1da6e06de7046fd1a
JordyNtambwe <jordyntambwe@gmail.com>
2024-03-05 15:48:06 -0700

    commit on master

--- eqObjects.js
+++ eqObjects.js
@@ -28,14 +28,14 @@ const eqArrays = function (array1, array2) {
 };
 
 const eqObjects = function(object1, object2) {
-const keys1 = Object.keys(object1) // [colors, size]
-const keys2 = Object.keys(object2) // [size, colors]
+const keys1 = Object.keys(object1) 
+const keys2 = Object.keys(object2)
 
 if(keys1.length !== keys2.length) { 
   return false
 }
 
-for(const key of keys1) { // key = colors, key = size
+for(const key of keys1) {
   if(Array.isArray(object1[key]) && Array.isArray(object2[key])) {
     if(!eqArrays(object1[key], object2[key])) {
       return false
