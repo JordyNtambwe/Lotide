9c178ca2cde6064a2acda8f5f60f53aa41951628
JordyNtambwe <jordyntambwe@gmail.com>
2024-03-04 18:21:33 -0700

    k

--- eqObjects.js
+++ eqObjects.js
@@ -29,14 +29,6 @@ const eqArrays = function (array1, array2) {
 
 const eqObjects = function(object1, object2) {
 
-// for(const keys of Object.keys(object1)) {
-//   if(Array.isArray(Object.keys(object1[keys])) !== Array.isArray(Object.keys(object2[keys])))
-//   if((Object.keys(object1)) !== (Object.keys(object2))) {
-//     return false
-//   } else {
-//     return true
-//   }
-// }
 const objectKeys = Object.keys(object1)
 for(const keys of objectKeys) {
   if(object1[keys] !== object2[keys]) {
@@ -45,7 +37,6 @@ for(const keys of objectKeys) {
   } else {
     return true
   }
-  return keys
 }
 
 }
