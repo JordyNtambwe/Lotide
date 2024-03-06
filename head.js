d76c2ee7cd3ca93d00c3727066f56940eeb289ba
JordyNtambwe <jordyntambwe@gmail.com>
2024-03-06 05:14:11 -0700

    commit on master

--- head.js
+++ head.js
@@ -1,14 +1,3 @@
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-};
-
-// The "head" function returns the first element of the array!
 const head = function(array) {
 
   if (array.length === 0) {
@@ -20,7 +9,4 @@ const head = function(array) {
 };
 
 
-
-// TEST CODES
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
