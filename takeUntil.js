1ccbf08ed9b15de618f3d33129f84c103af69230
JordyNtambwe <jordyntambwe@gmail.com>
2024-03-05 20:39:32 -0700

    commit on master

--- takeUntil.js
+++ takeUntil.js
@@ -7,7 +7,9 @@ const assertArraysEqual = function (array1, array2) {
   }
 }
 
-if (!array1 || !array2) {
+const eqArrays = function (array1, array2) {
+
+  if (!array1 || !array2) {
   return false;
   }
 
@@ -21,7 +23,28 @@ if (!array1 || !array2) {
     }
   }
   return true;
+  
+};
 
-  const takeUntil = function(array, callback) {
-    // ...
+const takeUntil = function(array, callback) {
+  let results = [];
+
+  for(let i = 0; i < array.length; i++) {
+    if(callback(array[i])) {
+      results = array.slice(0, i)
+      return results
+    }
   }
+  return results
+}
+
+// TEST CODE
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
