016005b748940723cd0e11764c8ab1d96326ad42
JordyNtambwe <jordyntambwe@gmail.com>
2024-03-06 00:52:11 -0700

    commit on master

--- takeUntil.js
+++ takeUntil.js
@@ -31,7 +31,7 @@ const takeUntil = function(array, callback) {
 
   for(let i = 0; i < array.length; i++) {
     if(callback(array[i])) {
-      results = array.slice(0, i);
+      return array.slice(0, i);
     }
   }
   return results
