ff364035849e28a44ecb777ba9c5098171584fee
JordyNtambwe <jordyntambwe@gmail.com>
2024-03-06 05:29:18 -0700

    commit on master

--- tail.js
+++ tail.js
@@ -1,13 +1,3 @@
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-};
-
 const tail = function(array) {
   return array.slice(1);
 };
