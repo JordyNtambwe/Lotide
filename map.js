6a18e76c302441b04e194a1b77d6f4c01a4e6a62
JordyNtambwe <jordyntambwe@gmail.com>
2024-03-05 20:15:10 -0700

    commit on master

--- map.js
+++ map.js
@@ -41,5 +41,5 @@ const words = ["ground", "control", "to", "major", "tom"];
 const results1 = map(words, word => word[0]);
 assertArraysEqual(results1, [ 'g', 'c', 't', 'm', 't' ]);
 
-const results3 = map(words, word => word.length)
-assertArraysEqual(results3, [6, 7, 2, 5, 3]);
+const results2 = map(words, word => word.length)
+assertArraysEqual(results2, [6, 7, 2, 5, 3]);
