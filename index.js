c29f4b2ed6190803376380bc5ad0aaeda01a1ecb
JordyNtambwe <152664833+JordyNtambwe@users.noreply.github.com>
2024-03-06 21:54:14 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-const assertArraysEqual  = require('./assertArraysEqual');
-const assertEqual        = require('./assertEqual');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
 const assertObjectsEqual = require('./assertObjectsEqual');
-const countLetters       = require('./countLetters');
-const countOnly          = require('./countOnly');
-const eqArrays           = require('./eqArrays');
-const eqObjects          = require('./eqObjects');
-const findKey            = require('./findKey');
-const findKeyByValue     = require('./findKeyByValue');
-const head               = require('./head');
-const letterPositions    = require('./letterPositions');
-const middle             = require('./middle');
-const tail               = require('./tail');
-const takeUntil          = require('./takeUntil');
-const without            = require('./without');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten')
+const head = require('./head');
+const letterPositions = require('./letterPositions');
+const middle = require('./middle');
+const tail = require('./tail');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
 
 module.exports = {
   assertArraysEqual,
@@ -24,6 +25,7 @@ module.exports = {
   eqObjects,
   findKey,
   findKeyByValue,
+  flatten,
   head,
   letterPositions,
   middle,
