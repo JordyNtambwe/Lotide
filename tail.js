e3ae5d1d50132f37f284acb8545443ac4d4e74e8
JordyNtambwe <152664833+JordyNtambwe@users.noreply.github.com>
2024-04-23 16:02:51 -0600

    Created a function that returns the "tail" of the array!

--- tail.js
+++ tail.js
@@ -10,17 +10,22 @@ const assertEqual = function(actual, expected) {
 
 const tail = function(array) {
 
-  return array.slice(1);
+  return array.slice(1)
 
-};
+}
+
 
 // Test Case: Check the original array
 const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+tail(words); 
+assertEqual(words.length, 3);
 
-// TEST CASE
+// Test Case #2
 const result = tail(["Hello", "Lighthouse", "Labs"]);
 assertEqual(result.length, 2);
 assertEqual(result[0], "Lighthouse");
 assertEqual(result[1], "Labs")
+
+// Test Case #3
+assertEqual(tail(["test"]).length, 0) 
+assertEqual(tail([]).length, 0)
