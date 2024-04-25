882698ec600a53135de3bba3d6ddb4f940cf0a29
JordyNtambwe <jordyntambwe@gmail.com>
2024-04-25 15:22:40 -0600

    Implement a function that takes in two arrays!

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -15,10 +15,13 @@ const eqArrays = function(array1, array2) {
 
 const assertArraysEqual = function(array1, array2) {
 
-  if (array1 === array2) {
+  if (eqArrays(array1, array2)) {
     console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
   } else {
     console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
   }
 
 };
+
+assertArraysEqual(eqArrays("Lighthouse Labs", "Bootcamp"), false);
+assertArraysEqual(eqArrays(1, 1), true);
