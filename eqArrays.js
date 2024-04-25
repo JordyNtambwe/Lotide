0b47de74e751e61995357b2182149b1ec33492f1
JordyNtambwe <jordyntambwe@gmail.com>
2024-04-24 19:46:30 -0600

    commit on master

--- eqArrays.js
+++ eqArrays.js
@@ -17,9 +17,9 @@ const eqArrays = function(array1, array2) {
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
       return false
-    } else {
-      return true
-    }
+    } 
+  } else {
+    return true
   }
 
 };
