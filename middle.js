68843f984fa0cb89468b60df5363e23f42af84f1
JordyNtambwe <jordyntambwe@gmail.com>
2024-04-25 21:49:57 -0600

    Implemented a function that will take in an array and return the middle-most element(s) of the given array!

---
+++ middle.js
@@ -0,0 +1,42 @@
+const assertArraysEqual = function(array1, array2) {
+
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+  }
+
+};
+
+const eqArrays = function(array1, array2) {
+
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    } 
+  }
+  return true;
+
+};
+
+const middle = function(array) {
+
+  const results = [];
+
+  if (array.length === 1 || array.length === 2) {
+    results = []
+  } else if (array.length % 2 === 0 && array.length > 2) {
+    results.push(array[(array.length / 2)] - 1);
+    results.push(array[(array.length / 2)]);
+  } else if (array.length % 2 === 1 && array.length > 2) {
+    results.push(array[Math.floor(array.length / 2)]);
+  }
+  return results;
+}
+
+console.log(middle([1, 2, 3, 4, 5]));
+console.log(middle([1, 2, 3, 4]));
