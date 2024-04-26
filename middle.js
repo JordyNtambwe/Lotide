ca8061bb2a3cd011a9a59bc518a16467347b90e0
JordyNtambwe <jordyntambwe@gmail.com>
2024-04-25 22:34:57 -0600

    Implemented a function that takes in an array and returns the middle-most element(s) of the given array!

--- middle.js
+++ middle.js
@@ -33,10 +33,9 @@ const middle = function(array) {
   }
 
   if (array.length % 2 === 0) {
-    results.push(array[(array.length / 2) - 1]);
-    results.push(array[(array.length / 2)]);
+    return [array[(array.length / 2) - 1], array[(array.length / 2)]];
   } else {
-    results.push(array[Math.floor(array.length / 2)]);
+    return [(array[Math.floor(array.length / 2)])];
   }
   return results;
 }
