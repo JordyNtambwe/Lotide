834fa39ed2876954fc5c27afe7171ac012253ee1
JordyNtambwe <jordyntambwe@gmail.com>
2024-04-25 22:55:22 -0600

    Implemented a function that takes in an array and returns the middle-most element(s) of the given array!

--- middle.js
+++ middle.js
@@ -23,16 +23,12 @@ const eqArrays = function(array1, array2) {
 
 };
 
-// The 'middle' function takes an array and returns an array containing the middle elements of the input array
 const middle = function(array) {
 
-// If the array has less than 3 elements, there is no 'middle' element(s), so return an empty array
   if (array.length <= 2) {
     return [];
   }
 
-// If the array length is even, return the two middle elements
-// If the array length is odd, return the single middle element
   if (array.length % 2 === 0) {
     return [array[(array.length / 2) - 1], array[(array.length / 2)]];
   } else {
