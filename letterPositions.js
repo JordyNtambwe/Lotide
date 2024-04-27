4ad2ff0bb0758e9173beeb89bae07a157b652e3c
JordyNtambwe <jordyntambwe@gmail.com>
2024-04-27 17:42:03 -0600

    k

--- letterPositions.js
+++ letterPositions.js
@@ -29,6 +29,11 @@ const letterPositions = function (sentence) {
   
   for (let i = 0; i < sentence.length; i++) {
     if (sentence[i] !== " ") {
+      if (results(sentence[i])) {
+        results(sentence[i]).push(i)
+      } else {
+        results(sentence[i]) = [i]
+      }
     }
   return results;
   };
