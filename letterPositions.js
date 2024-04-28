234902951397060eedf10f9750fb9cef41b05e8a
JordyNtambwe <jordyntambwe@gmail.com>
2024-04-27 18:23:30 -0600

    Created a function that returns all the indices (zero-based positions in the string where each character is found!

--- letterPositions.js
+++ letterPositions.js
@@ -28,7 +28,7 @@ const letterPositions = function (sentence) {
   const results = {};
   
   for (let i = 0; i < sentence.length; i++) {
-    let character = sentence[i];
+    const character = sentence[i];
     if (character !== " ") {
       if (results[character]) {
         results[character].push(i)
