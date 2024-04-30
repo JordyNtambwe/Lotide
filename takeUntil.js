737eb08e1c585b6fa992151429dfa4cbd9c0e3fe
JordyNtambwe <jordyntambwe@gmail.com>
2024-04-29 20:21:54 -0600

    j

---
+++ takeUntil.js
@@ -0,0 +1,35 @@
+const assertArraysEqual = function(array1, array2) {
+
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+  }
+
+};
+
+const eqArrays = function(array1, array2) {
+
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    } 
+  }
+  return true;
+
+};
+
+const takeUntil = function(array, callback) {
+
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i])) {
+      array.splice(i);
+      return array;
+    }
+  }
+
+};
