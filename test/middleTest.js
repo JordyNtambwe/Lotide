8e03d3c6c4e432c62f9a8e926906941fff6e8bb0
JordyNtambwe <jordyntambwe@gmail.com>
2024-05-12 21:41:07 -0600

    Incorporated Mocha & Chai to tailTest.js & middleTest.js!

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,13 @@
+const assert = require('chai').assert;
 const assertArraysEqual = require("../assertArraysEqual");
 const middle = require("../middle");
 
-// TEST CODE
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2]), []);
+describe("#middle", () => {
+  it("returns 2 for [1, 2, 3]", () => {
+    assert.strictEqual(assertArraysEqual(middle([1, 2, 3]), [2]));
+  });
+  it("returns [] for ['5']", () => {
+    assert.strictEqual( assertArraysEqual(middle([1, 2]), []));
+  });
+
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,17 +1,19 @@
+const assert = require('chai').assert;
 const tail = require("../tail");
 const assertEqual = require("../assertEqual");
 
-// Test Case #1: Check the original array
 const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-// Test Case #2: Check the returned array
 const results = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(results.length, 2); 
-assertEqual(results[0], "Lighthouse");
-assertEqual(results[1], "Labs"); 
 
-// Test Case #3: Check arrays with one or zero elements
-assertEqual(tail(["test"]).length, 0);
-assertEqual(tail([]).length, 0);
+describe("#tail", () => {
+  it("returns 3 for [Yo Yo, Lighthouse, Labs]", () => {
+    assert.strictEqual(assertEqual(words.length, 3));
+  });
+  it("returns Lighthouse for tail([Hello, Lighthouse, Labs])", () => {
+    assert.strictEqual(assertEqual(results[1], "Labs"));
+  });
+  it("returns 0 for tail([Hello, Lighthouse, Labs])", () => {
+    assert.strictEqual(assertEqual(tail([]).length, 0));
+  });
+
+});
