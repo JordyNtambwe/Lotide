68049cebd06f079ce819753ab2e5052a50da577d
JordyNtambwe <jordyntambwe@gmail.com>
2024-05-12 21:14:54 -0600

    Refactored middle.js!

--- middle.js
+++ middle.js
@@ -1,3 +1,4 @@
+// This function returns the middle elements of an array. If the array has an even number of elements, it returns the two middle elements. If the array has an odd number of elements, it returns the single middle element.
 const middle = function(array) {
 
   if (array.length <= 2) {
