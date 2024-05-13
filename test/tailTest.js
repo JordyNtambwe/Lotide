a5e685f39e3b6acc8c11342bee6288d8ba43c644
JordyNtambwe <jordyntambwe@gmail.com>
2024-05-12 22:27:25 -0600

    Incorporated Mocha & Chai to middleTest.js & tailTest.js!

--- test/tailTest.js
+++ test/tailTest.js
@@ -2,7 +2,6 @@ const assert = require('chai').assert;
 const tail = require("../tail");
 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
-const results = tail(["Hello", "Lighthouse", "Labs"]);
 
 describe("#tail", () => {
   it("original array length remains the same after tail function is called", () => {
@@ -10,6 +9,7 @@ describe("#tail", () => {
     assert.deepEqual(words.length, 3);
   });
   it("returns ['Lighthouse', 'Labs'] for tail(['Hello', 'Lighthouse', 'Labs'])", () => {
+    const results = tail(["Hello", "Lighthouse", "Labs"]);
     assert.deepEqual(results, ["Lighthouse", "Labs"]);
 });
   it("returns 0 for tail([]).length", () => {
